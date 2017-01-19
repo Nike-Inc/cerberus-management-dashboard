7f376457ef92451f80acdf9b8ec669004e1c1857
sford4 <shaun.ford@nike.com>
2017-01-18 17:24:29 -0800

    remove log statement in actions

--- app/actions/statsActions.js
+++ app/actions/statsActions.js
@@ -16,7 +16,6 @@ export function fetchStats(token) {
         })
         .then(function (response) {
             let stats = response.data
-            console.log(JSON.stringify(stats, null, 4))
             if (stats) {
                 stats['safe_deposit_box_meta_data'].sort((a, b) => {
                     let dateA =  new Date(a['last_updated_ts']).getTime()
