1d24590bc805fedd2cd134104493ee390d9c28b5
Shaun Ford <shaun.ford@nike.com>
2017-03-24 11:27:33 -0700

    Add autofocus to MFA secure code input

--- app/components/LoginMfaForm/LoginMfaForm.js
+++ app/components/LoginMfaForm/LoginMfaForm.js
@@ -81,6 +81,7 @@ export default class LoginMfaForm extends Component {
                                 <input type='text'
                                        className='ncss-input pt2-sm pr4-sm pb2-sm pl4-sm r'
                                        placeholder='Please enter your OTP token'
+                                       autoFocus={true}
                                        {...otpToken}/>
                                 {otpToken.touched && otpToken.error && <div className='ncss-error-msg'>{otpToken.error}</div>}
                             </div>
