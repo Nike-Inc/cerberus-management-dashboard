36bb80e949f7e7122036963161143d34c521493f
melanahammel <melana.hammel@nike.com>
2017-06-29 17:18:22 -0700

    added renew token functionality, animation in progress

--- app/components/ViewTokenModal/ViewTokenModal.js
+++ app/components/ViewTokenModal/ViewTokenModal.js
@@ -4,59 +4,121 @@ import { connect } from 'react-redux'
 import { reduxForm, touch } from 'redux-form'
 import * as modalActions from '../../actions/modalActions'
 import * as appActions from '../../actions/appActions'
+import * as authActions from '../../actions/authenticationActions'
+import CopyToClipboard from 'react-copy-to-clipboard';
+import ProgressButton from 'react-progress-button'
 
+
+import * as messengerActions from '../../actions/messengerActions'
+
+
+
+// Some code from https://github.com/mathieudutour/react-progress-button
 
 import './ViewTokenModal.scss'
 
-
 // connect to the store in order to obtain the current vault token
 @connect((state) => {
-  console.log(state.auth.vaultToken)
-    return {
-        vaultToken: state.auth.vaultToken,
+  return {
+    clientToken: state.auth.vaultToken,
+  }
+})
 
-      }
-  })
+
 
 
 export default class ViewTokenModal extends Component {
 
+  render() {
+    const {clientToken, dispatch} = this.props
+    let tokenExpiresDate = sessionStorage.getItem('tokenExpiresDate');
 
+    console.log(dateDiffinMin(tokenExpiresDate))
 
-    render() {
-      const {vaultToken, dispatch} = this.props
-      let tokenExpiresDate = sessionStorage.getItem('tokenExpiresDate');
-
-
-      return (
-  <div>
+    return (
+      <div>
         <div id="form-description" className="ncss-brand">
-            <h1>View and Refresh Vault Token</h1>
-            <h4>View your current vault token which serves this purpose. Refresh your vault token for these reasons. </h4>
-        {/* </div>
-        <div  id="form-description" className="ncss-brand"> */}
-          <h3>Vault Token:</h3>
-              <p>{ vaultToken }</p>
-                <h3>Vault Token Expiration Date:</h3>
-                <p>{tokenExpiresDate }</p>
-          </div>
+          <h1>View and Renew Client Token</h1>
+          <h4>This is your current client token, export it and use it with your client to do this. Renew your client token for these reasons. </h4>
 
-          <div id="refresh-btn-container">
-              <div id='cancel-btn'
-                   className='btn ncss-btn-dark-grey ncss-brand pt3-sm pr5-sm pb3-sm pl5-sm pt2-lg pb2-lg u-uppercase'
-                   onClick={ () => {
-                          dispatch(modalActions.popModal())
-                      }}>Cancel
-              </div>
-              <button id='refresh-btn'
-                      className='btn ncss-btn-dark-grey ncss-brand pt3-sm pr5-sm pb3-sm pl5-sm pt2-lg pb2-lg u-uppercase'
-                      // disabled={isSubmitting}
-                      >Refresh
-              </button>
-          </div>
+          <h3>Client Token:</h3>
+          <p>{ clientToken }</p>
+
+          <div className='row-buttons'>
+            {/* <div className="btn-wrapper btn-wrapper-left">
+            <input type="checkbox" className={! entry.revealed.value ? 'row-btn row-btn-reveal' : 'row-btn row-btn-revealed'} {...entry.revealed}/>
+          </div> */}
+          <CopyToClipboard text={clientToken}>
+            {/* // what does this line do?  */}
+            <div className={clientToken.length <= 1 ? 'btn-wrapper btn-wrapper-right' : 'btn-wrapper'}>
+              <div className='row-btn row-btn-copy'></div>
+            </div>
+          </CopyToClipboard>
+        </div>
+
+
+        <h3>Client Token Expiration Date:</h3>
+        <p>{tokenExpiresDate }</p>
+        <h3>Client Token Time Remaining:</h3>
+        <p> {dateDiffinMin(tokenExpiresDate)} minutes</p>
+      </div>
+
+      <div id="renew-btn-container">
+        <div id='close-btn'
+          className='btn ncss-btn-dark-grey ncss-brand pt3-sm pr5-sm pb3-sm pl5-sm pt2-lg pb2-lg u-uppercase'
+          onClick={ () => {
+            dispatch(modalActions.popModal())
+          }}>Close
+        </div>
+
+        <ProgressButton ref='button' //id='renew-btn'
+        className='btn ncss-btn-dark-grey ncss-brand pt3-sm pr5-sm pb3-sm pl5-sm pt2-lg pb2-lg u-uppercase'
+
+        // onClick={ () => {
+        //   dispatch(authActions.refreshAuth(clientToken, '', false));
+        //   //  dispatch(messengerActions.addNewMessageWithTimeout('Your token has been renewed', 10000000))
+        //
+        //   // console.log("testing renew");
+        //
+        //   return new Promise(function(resolve, reject) {
+        //     setTimeout(resolve, 3000)
+        //   })
+        //
+        // }}
+        onClick={this.handleRefreshTokenClick}
+        >Renew Token
+      </ProgressButton>
+    </div>
   </div>
 
-      )
-    }
+)
+}
 
+handleRefreshTokenClick() {
+    const {clientToken, dispatch} = this.props
+   dispatch(authActions.refreshAuth(clientToken, '', false));
+  return new Promise(function(resolve, reject) {
+    setTimeout(resolve, 3000)
+  })
+}
+
+// handleClick() {
+//     this.refs.button.loading();
+//     console.log("loading")
+//     //make asynchronious call
+//     setTimeout(function() {
+//       this.refs.button.success();
+//         console.log("success")
+//     }.bind(this), 3000);
+//   }
+
+}
+
+export function dateDiffinMin(expiresDate) {
+  var expDate = new Date(expiresDate)
+  var currentDate = new Date()
+  var t2 = expDate.getTime();
+  var t1 = currentDate.getTime();
+  var diff = parseInt((t2-t1)/(60*1000));
+  return diff
 }
